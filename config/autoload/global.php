b5e4968a87157afe9a4b7cb15e82490912e5daa3
mc_bnn <mc_bnn@localhost>
2017-10-12 09:57:16 +0300

    content

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -3,7 +3,6 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=mcbnn_booknew;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
