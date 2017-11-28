26bae36d4b71cf7bb4e76ac31e472bcbbd4373ff
mcbnn <zeshka123>
2017-11-28 14:13:10 +0300

    парсинг

--- module/Application/src/Application/Controller/ParserController.php
+++ module/Application/src/Application/Controller/ParserController.php
@@ -113,7 +113,8 @@ class ParserController {
     public function parser($sm) {
         $this->sm = $sm;
         ini_set('max_execution_time', 100000);
-
+        error_reporting(E_ALL);
+        display_errors(1);
         for ($m = 17237; $m >= 1; $m--) {
             //
             echo $m;
