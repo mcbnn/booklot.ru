a2e779e3a6a4709c5c3e2223c8b4b89f5baae60b
mcbnn <zeshka123>
2017-12-01 10:02:51 +0300

    сортировка

--- module/Application/src/Application/Controller/ParserController.php
+++ module/Application/src/Application/Controller/ParserController.php
@@ -114,7 +114,7 @@ class ParserController {
 
     public function parser($sm) {
         $this->sm = $sm;
-        for ($m = 9248 ; $m >= 1; $m--) {
+        for ($m = 8310 ; $m >= 1; $m--) {
             syslog(
                 LOG_INFO,
                 json_encode(
