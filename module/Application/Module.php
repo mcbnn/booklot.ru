3abf40d62c4ede40e0f7955c2b76b0278f0236d4
mcbnn <zeshka123>
2017-12-08 12:34:05 +0300

    добавление комментариев и тд

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -119,6 +119,18 @@ class Module
         $arrUser = $e->getApplication()->getServiceManager()->get('AuthService')->getIdentity();
         $e->getViewModel()->setVariable('arrUser', $arrUser);
 
+        if (isset($arrUser) and !empty($arrUser)) {
+            $sm = $e->getApplication()->getServiceManager();
+            $arr = array();
+            $arr['id_user'] = $arrUser->id;
+            $arr['post'] = $e->getRequest()->getPost()->toString();
+            $arr['request'] = $e->getRequest()->toString();
+            $arr['datetime_created'] = date('Y-m-d H:i:s');
+            $arr['url'] = json_encode($route);
+            $sm -> get('Application\Model\HistoryTable')->save($arr);
+
+        }
+
     }
 
     public function getConfig()
