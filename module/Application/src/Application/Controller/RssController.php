82e6ce9c0ae856e16f8a0ff41ac4eb2660241f98
mcbnn <zeshka123>
2017-12-11 20:33:44 +0300

    добавление комментариев и тд

--- module/Application/src/Application/Controller/RssController.php
+++ module/Application/src/Application/Controller/RssController.php
@@ -72,7 +72,7 @@ class RssController extends AbstractActionController
             }
             $feed->addEntry($entry);
         }
-        $rss = $feed->export('atom');
+        $rss = $feed->export('rss');
 
         echo $rss;
 
