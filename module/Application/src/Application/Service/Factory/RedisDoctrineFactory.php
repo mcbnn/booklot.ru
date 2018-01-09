f80d6e27d5c2b98d6c8eddbb6c95c50affeb5efe
mcbnn <zeshka123>
2018-01-10 02:17:06 +0300

    zf2->zf3

--- module/Application/src/Application/Service/Factory/RedisDoctrineFactory.php
+++ module/Application/src/Application/Service/Factory/RedisDoctrineFactory.php
@@ -1,13 +1,15 @@
 <?php
 namespace Application\Service\Factory;
 
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 use DoctrineModule\Cache\ZendStorageCache;
 
 class RedisDoctrineFactory implements FactoryInterface {
 
-    public function createService(ServiceLocatorInterface $serviceLocator) {
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $serviceLocator = $container->get('ServiceManager');
         $redis_cache = $serviceLocator->get('Application\Cache\Redis');
         $doctrine_cache = new ZendStorageCache($redis_cache);
         return $doctrine_cache;
