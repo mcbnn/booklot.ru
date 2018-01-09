5ceae0e3e1890d34702ba4e440761a26b94f8e50
mcbnn <zeshka123>
2018-01-09 14:18:24 +0300

    no display errors

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,31 +1,27 @@
 <?php
 
-return array(
-    'db' => array(
+return [
+    'db' => [
         'driver'         => 'Pdo',
-        'driver_options' => array(
+        'driver_options' => [
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ),
-    ),
-    'service_manager' => array(
-        'factories' => array(
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
             'AdapterFirst' => 'Zend\Db\Adapter\AdapterServiceFactory',
             'admin_menu' => 'Admin\Model\SecondaryNavigationFactory',
 			'Adapter' => function ($sm) {
 				$config = $sm->get('Config');
 				return new Zend\Db\Adapter\Adapter($config['db']);
 			},
-        ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => false,
-        'display_exceptions'       => false,
-    ),
-    'session' => array(
+        ],
+    ],
+    'session' => [
         'cookie_lifetime' => 2419200, //SEE ME
         'remember_me_seconds' => 2419200, //SEE ME
         'use_cookies' => true,
         'cookie_httponly' => true,
-    )
-);
+    ]
+];
