001668fd09f726430f7f0942448a0a9764acec59
mcbnn <mc_bnn@mail.ru>
2018-03-20 11:02:39 +0300

    change route bad

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -342,7 +342,7 @@ return [
                                                 'options' => [
                                                     'route'       => 'content/[:content/]',
                                                     'constraints' => [
-                                                        'content' => '[0-9][a-zA-Z0-9-]*',
+                                                        'content' => '[a-zA-Z0-9-]{1,}',
                                                     ],
                                                     'defaults'    => [
                                                         'controller' => Controller\IndexController::class,
