a40b53a7780ab30d5f509f2d98d09600d9828248
mcbnn <mc_bnn@mail.ru>
2018-03-21 13:58:56 +0300

    check foto

--- module/Application/src/Application/Controller/TechnicalController.php
+++ module/Application/src/Application/Controller/TechnicalController.php
@@ -89,7 +89,7 @@ class TechnicalController extends AbstractActionController
     {
         $config = $this->sm->get('config');
         $em = $this->getEntityManager();
-        $book  = $em->getRepository(Book::class)->findBy([], [], 100);
+        $book  = $em->getRepository(Book::class)->findBy([], [], 200);
         foreach($book as $item){
             if($item->getFoto() == 'nofoto.jpg')continue;
             $src = $config['UPLOAD_DIR'].'newimg/original/'.$item->getFoto();
