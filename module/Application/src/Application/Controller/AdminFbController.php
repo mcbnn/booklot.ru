8de46464131a2934257fe0d21e3738b2ce83bac2
mcbnn <mc_bnn@mail.ru>
2018-03-26 12:34:47 +0300

    mail

--- module/Application/src/Application/Controller/AdminFbController.php
+++ module/Application/src/Application/Controller/AdminFbController.php
@@ -76,7 +76,7 @@ class AdminFbController extends AbstractActionController
         }
         $documentFb2 = new DocumentFb2($this->getEntityManager(), $this->sm);
         $documentFb2->file_id = $id;
-        set_time_limit(12);
+        set_time_limit(50);
         $messages = $documentFb2->convert($doc);
         $this->flashMessenger()->addMessage($messages->getError());
         return $this->redirect()->toRoute(
