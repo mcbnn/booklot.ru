ddf5439ff19ca5cb9949b6115147fe2e4d02d249
mcbnn <mc_bnn@mail.ru>
2018-04-13 12:01:36 +0300

    change menu

--- module/Application/src/Application/Entity/Text.php
+++ module/Application/src/Application/Entity/Text.php
@@ -110,5 +110,76 @@ class Text
         $this->idMain = $idMain;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getTextImgServer()
+    {
+        return $this->changeImageServer()->ins_adv()->text;
+    }
+
+    /**
+     * Изменение фото в тексте
+     * @return $this
+     */
+    public function changeImageServer()
+    {
+        $this->text = preg_replace_callback(
+            '/\<img[\s]*src[\s]*\=[\s]*"(.*)"[\s]*>/isU',
+            function ($matches) {
+                $file = explode('/', $matches[1]);
+                $file = end($file);
+                return '<img src = "https://www.image.booklot.ru/resize/200/'.$file.'" >';
+            },
+            $this->text
+        );
+        return $this;
+    }
+
+    /**
+     * Добавление рекламы google в текст
+     * @return mixed|string
+     */
+    public function ins_adv()
+    {
+        $text = $this->text;
+        if (!$text) {
+            $this->text = "<div class = 'alert alert-warning'>Сайт находится в наполнение, по поводу замечаний пишите на почту <a href = 'mailto:mc_bnn@mail.ru?subject=Вопрос по замечанию на сайте'>mc_bnn@mail.ru</a></div>";
+            return $this;
+        }
+        $text = preg_replace("/\<a(.*)\>(.*)\<\/a\>/iU", "$2", $text);
+        $txt = '';
+        $i = 2;
+        $block
+            = "<div class = 'text-center'><script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+<ins class=\"adsbygoogle\"
+     style=\"display:block\"
+     data-ad-client=\"ca-pub-2745956118385780\"
+     data-ad-slot=\"3283834548\"
+     data-ad-format=\"auto\"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>";
+        $arr = array();
+        $arr1 = array();
+        $tag = "</p>";
+        $pieces = explode($tag, $text);
+        foreach ($pieces as $piece) {
+            $txt .= $piece;
+            if (strlen(strip_tags($txt)) > 5000) {
+                // добавляем в конец разделитель $tag,
+                // т.к. в массив попадают строки без него.
+                $arr[] = substr($piece, -250).$tag;
+                $arr1[] = substr($piece, -250).$tag.$block;
+                $txt = '';
+                $i += 1;
+            }
+            if ($i == 1) {
+                break;
+            }
+        }
+        $this->text = str_replace($arr, $arr1, $text);
+        return $this;
+    }
 
 }
