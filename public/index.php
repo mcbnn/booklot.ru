b81b7bf3884899f83e5492af38575e7782e75ef5
mcbnn <mc_bnn@mail.ru>
2018-05-21 10:54:57 +0300

    fix bug

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+$ru = $_SERVER['REQUEST_URI'];
+if (preg_match('/[A-Z]/', $ru)) {
+    $ru = strtolower($ru);
+    header("Location: $ru",TRUE,301);
+    exit();
+}
 chdir(dirname(__DIR__));
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
