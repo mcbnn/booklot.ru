7081c2bc209666b6a86f4a3f7705140f27a4b694
Николай Бодров <bodrov@simicon.com>
2018-11-13 13:46:29 +0300

    clear

--- module/Application/src/Application/Controller/AuthController.php
+++ module/Application/src/Application/Controller/AuthController.php
@@ -111,7 +111,7 @@ class AuthController extends AbstractActionController
                 $status['getLine'] = $e->getLine();
                 $status['getMessage'] = $e->getMessage();
                 $status['trace'] = $e->getTraceAsString();
-                $status['all'] = $e;
+                $status['code'] = $e->getCode();
             }
 
 			echo json_encode($status);die();
