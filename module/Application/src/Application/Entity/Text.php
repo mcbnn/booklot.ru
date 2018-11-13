80a39e63326f0f03778c39c1c41c6e2fb018d373
Николай Бодров <bodrov@simicon.com>
2018-11-13 11:15:59 +0300

    clear

--- module/Application/src/Application/Entity/Text.php
+++ module/Application/src/Application/Entity/Text.php
@@ -126,16 +126,16 @@ class Text
     {
         if(isset($_GET['test'])){
             $this->text = preg_replace_callback(
-                '/\<img.{0,300}src[\s]*\=[\s]*"(.*)"[\s]*>/isU',
+                '/\<img.{0,300}src[\s]*\=[\s]*(\"|\')(.*)(\"|\')[\s]*>/isU',
                 function ($matches) {
-
+                    var_dump( $matches);die();
                     $file = explode('/', $matches[1]);
                     $file = end($file);
                     return '<img src = "'.IMAGE_URL.'/resize/200/'.$file.'" >';
                 },
                 $this->text
             );
-            var_dump(  $this->text);die();
+
            die();
         }
         $this->text = preg_replace_callback(
