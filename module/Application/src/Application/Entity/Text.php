465af3d75fd123d7adc3eaad02f7facf6611abb5
Николай Бодров <bodrov@simicon.com>
2018-11-13 11:15:11 +0300

    clear

--- module/Application/src/Application/Entity/Text.php
+++ module/Application/src/Application/Entity/Text.php
@@ -128,13 +128,14 @@ class Text
             $this->text = preg_replace_callback(
                 '/\<img.{0,300}src[\s]*\=[\s]*"(.*)"[\s]*>/isU',
                 function ($matches) {
-                    var_dump($matches);die();
+
                     $file = explode('/', $matches[1]);
                     $file = end($file);
                     return '<img src = "'.IMAGE_URL.'/resize/200/'.$file.'" >';
                 },
                 $this->text
             );
+            var_dump(  $this->text);die();
            die();
         }
         $this->text = preg_replace_callback(
