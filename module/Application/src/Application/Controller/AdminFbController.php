826c47eabedb3cdf2d8a642f8fc5423daefab460
Николай Бодров <bodrov@simicon.com>
2018-11-21 15:58:42 +0300

    clear

--- module/Application/src/Application/Controller/AdminFbController.php
+++ module/Application/src/Application/Controller/AdminFbController.php
@@ -214,10 +214,7 @@ class AdminFbController extends AbstractActionController
                         $nameFile = substr($nameFile, 0, 50).'.fb2';
                         $upload_dir = $config['UPLOAD_DIR'];
                         $upload_file = $upload_dir.'newsave/convert/'.$nameFile;
-                        var_dump(move_uploaded_file(
-	                        $file['tmp_name'],
-	                        $upload_file
-                        ));
+                        var_dump( $file['tmp_name']);
                         if (!move_uploaded_file(
                             $file['tmp_name'],
                             $upload_file
