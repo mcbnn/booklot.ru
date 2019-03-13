66358047a9971b9bfe9ffc5bc20043210dc6cbfb
Николай Бодров <bodrov@simicon.com>
2019-03-13 11:30:35 +0300

    fix-bug

--- module/Application/src/Application/Model/DocumentFb2.php
+++ module/Application/src/Application/Model/DocumentFb2.php
@@ -410,6 +410,7 @@ class DocumentFb2
                             if ($k > 1) {
                                 continue;
                             }
+                            var_dump($v);
                             $textTitle['title'][] = strip_tags($v);
                         }
                     };
@@ -444,7 +445,6 @@ class DocumentFb2
         }
         while($strlen);
         $this->text = $arrText;
-        var_dump(111);
         die();
     }
 
