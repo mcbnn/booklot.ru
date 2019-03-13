52c1ff0be30411c688096aae2a32d9953dcf60a4
Николай Бодров <bodrov@simicon.com>
2019-03-13 11:17:04 +0300

    fix-bug

--- module/Application/src/Application/Model/DocumentFb2.php
+++ module/Application/src/Application/Model/DocumentFb2.php
@@ -310,6 +310,7 @@ class DocumentFb2
                 $ar['link'] = preg_replace("/[^a-zA-Z_0-9]/iu", '', $v[1]);
                 $ar['title'] = "";
                 if(preg_match_all('/<title>(.*)<\/title>/isU', $v[2], $title)){
+                    $title[1][0] = preg_replace('/<\/p>[\s]*<p>/isU', '</p>&nbsp;&nbsp;<p>', $title[1][0]);
                     $ar['title'] = strip_tags($title[1][0]);
                 }
 
